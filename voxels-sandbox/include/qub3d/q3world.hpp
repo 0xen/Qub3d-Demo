ab5bb0b3e6f310f46d3b1b7b9c006bc93335c0d5
Barney Wilks <barneywilks1@gmail.com>
2018-07-09 21:35:36 +0100

    Implemented placing blocks and commented the world code.

--- voxels-sandbox/include/qub3d/q3world.hpp
+++ voxels-sandbox/include/qub3d/q3world.hpp
@@ -8,22 +8,61 @@
 #include <qub3d/q3camera.hpp>
 #include <qub3d/q3window.hpp>
 
-struct Q3Block {
-    glm::vec3 position;
-    bool exists = true;
+#include <array>
+
+/*
+    The type of the block. A property of Q3Block
+*/
+enum class Q3BlockType 
+{
+    Air, Grass
 };
 
-class Q3World {
+/*
+    Stores data about each block, such as its position or type.
+    Each block is 2x2x2 in size.
+*/
+struct Q3Block 
+{
+    glm::vec3 position;
+    Q3BlockType type = Q3BlockType::Air;
+};
+
+/*
+Represents the game world, storing block data and handling voxel manipulation.
+Can be rendered via the Q3WorldRenderer class.
+*/
+class Q3World 
+{
 public:
+    typedef std::array<std::array<std::array<Q3Block, Q3_MAPSIZE>, Q3_MAPSIZE>, Q3_MAPSIZE> Q3BlocksArray;
+
+    /* Generate the worlds terrain. This includes air. Can be called multiple times to regenerate the world. */
     void generate();
+
+    /* Update the world. Includes handling world editing such as placing/destroying blocks. */
     void tick(Q3Camera *camera, Q3Window *window);
-    Q3Block *getBlocks() { return m_blocks; }
+
+    /* Get the 3D array structure containing the block data. */
+    Q3BlocksArray& getBlocks() { return m_blocks; }
 
 private:
-    Q3Block m_blocks[Q3_MAPSIZE * Q3_MAPSIZE];
+    /* 
+        Return the block at the specified position. 
+        If ignoreAir = true then it will only return blocks that are not air.
+
+        If there is no block at the specified position OR ignoreAir is true and it is an air block at that position, return NULL.
+    */
+    Q3Block * getBlockAtPos(glm::vec3 pos, bool ignoreAir);
+
+private:
+
+    Q3BlocksArray m_blocks;
 };
 
-class Q3WorldRenderer {
+class Q3WorldRenderer 
+{
 public:
+    /* Draw the world to the current OpenGL context. */
     void render(Q3World *world, Q3Renderer *renderer);
 };
--- voxels-sandbox/src/q3world.cc
+++ voxels-sandbox/src/q3world.cc
@@ -1,84 +1,200 @@
 #include <qub3d/q3world.hpp>
-
 #include <qub3d/q3perlin.hpp>
+
 #include <iostream>
+#include <vector>
+#include <functional>
+
 void Q3World::generate()
 {
-    memset(m_blocks, 0, Q3_MAPSIZE * Q3_MAPSIZE);
+    // Lets fill the world out with empty air blocks before generating any terrain.
+    for (int y = 0; y < Q3_MAPSIZE; ++y) 
+    {
+        for (int z = 0; z < Q3_MAPSIZE; ++z) 
+        {
+            for (int x = 0; x < Q3_MAPSIZE; ++x) 
+            {
+                Q3Block *block = &(m_blocks[x][y][z]);
+                block->position.x = x * 2;
+                block->position.y = y * 2;
+                block->position.z = z * 2;
+            }
+        }
+    }
+    
+    /*
+     Now lets actually generate the terrain blocks.
+     Only places the top layer for now.
+    */
     Q3PerlinNoiseGenerator perlin;
-    for (int z = 0; z < Q3_MAPSIZE; ++z) {
-        for (int x = 0; x < Q3_MAPSIZE; ++x) {
-            Q3Block *block = &(m_blocks[x + z * Q3_MAPSIZE]);
-            block->position.x = std::round(+(x * 2.f));
-            block->position.z = std::round(+(z * 2.f));
-
+    for (int z = 0; z < Q3_MAPSIZE; ++z) 
+    {
+        for (int x = 0; x < Q3_MAPSIZE; ++x) 
+        {
+            // Don't ask how this works. Please.
             float normalized_perlin = perlin.perlin(glm::vec3((x * 2.f) / 10.f, (z * 2.f) / 10.f, 0.f));
             float scaled_perlin = normalized_perlin * 15.f;
             float rounded_scaled_perlin = static_cast<float>((int)(scaled_perlin / 2) * 2);
 
+            Q3Block *block = &(m_blocks[x][rounded_scaled_perlin / 2][z]);
+
             block->position.y = rounded_scaled_perlin;
+            block->type = Q3BlockType::Grass;
+            block->position.x = std::round(+(x * 2.f));
+            block->position.z = std::round(+(z * 2.f));
         }
     }
 }
 
-bool s_wasClicked = false;
+// These are used to keep track of is the left and right mouse buttons are being held down or not.
+// Useful when we only need to do an action when a button is first placed
+bool s_right_button_was_clicked = false;
+bool s_left_button_was_clicked = false;
+
+void doOnce(bool condition, bool& flag, std::function<void()> action) 
+{
+    if (condition) 
+    {
+        if (!flag) 
+        {
+            action();
+            flag = true;
+        }
+    }
+    else 
+    {
+        flag = false;
+    }
+}
+
 void Q3World::tick(Q3Camera * camera, Q3Window * window)
 {
     glm::vec3 pos = camera->getPosition() + camera->getDirection();
     
-    int mouse_x, mouse_y;
-    Uint32 mouse_state = SDL_GetMouseState(&mouse_x, &mouse_y);
+    Uint32 mouse_state = SDL_GetMouseState(NULL, NULL);
     
-    if (mouse_state & SDL_BUTTON(SDL_BUTTON_LEFT)) {
-        if (!s_wasClicked) {
-            const float INC = 0.5f;
-            
-            float len = 0.f;
-            Q3Block *hit = nullptr;
+    const float RAY_INCREMENT = 0.5f;
+    const float PICKING_RANGE = 32.f;
 
-            while (len < 32.f && !hit) {
-                pos += camera->getDirection() * INC;
+    // Only do this when Q3_MOUSE_BUTTON_PLACE_BLOCK is first pressed
+    doOnce(mouse_state & SDL_BUTTON(Q3_MOUSE_BUTTON_PLACE_BLOCK), s_left_button_was_clicked,
+        [&]()
+        {
+            float ray_length = 0.f;
 
-                for (int i = 0; i < Q3_MAPSIZE * Q3_MAPSIZE; ++i) {
-                    Q3Block *block = &(m_blocks[i]);
-                    glm::vec3 bpos = block->position;
+            while (ray_length < PICKING_RANGE) 
+            {
+                // Advance the ray in the direction that the camera is facing.
+                pos += camera->getDirection() * RAY_INCREMENT;
 
-                    if (!block->exists)
-                        continue;
+                /*
+                    If we hit a non air block, move to the last position of the ray before entering that block
+                    then place a block at that position.
+                */
+                Q3Block *hit = getBlockAtPos(pos, true);
+                if (hit != nullptr) 
+                {
+                    pos -= camera->getDirection() * RAY_INCREMENT;
+                    hit = getBlockAtPos(pos, false);
 
-                    if (pos.x > bpos.x - 1.f && pos.x < bpos.x + 1.f) {
-                        if (pos.y > bpos.y - 1.f && pos.y < bpos.y + 1.f) {
-                            if (pos.z > bpos.z - 1.f && pos.z < bpos.z + 1.f) {
-                                hit = block;
-                                break;
-                            }
-                        }
+                    if (hit != nullptr) 
+                    {
+                        hit->type = Q3BlockType::Grass;
+                        break;
                     }
                 }
 
-                if (hit) {
-                    hit->exists = false;
-                }
-                
-                len += INC;
+                ray_length += RAY_INCREMENT;
             }
+        }
+    );
 
-            s_wasClicked = true;
+    // Only do this when Q3_MOUSE_BUTTON_DESTROY_BLOCK is first pressed
+    doOnce(mouse_state & SDL_BUTTON(Q3_MOUSE_BUTTON_DESTROY_BLOCK), s_right_button_was_clicked,
+        [&]() 
+        { 
+            float ray_length = 0.f;
+
+            while (ray_length < PICKING_RANGE) 
+            {
+                // Advance the ray in the direction the camera is facing.
+                pos += camera->getDirection() * RAY_INCREMENT;
+
+                /*
+                    Get any non-air block at the current position of the ray and if
+                    there is one, destroy it (by making it an air block)
+                */
+                Q3Block *hit = getBlockAtPos(pos, true);
+                if (hit != nullptr) 
+                {
+                    hit->type = Q3BlockType::Air;
+                    break;
+                }
+
+                ray_length += RAY_INCREMENT;
+            }
+        }
+    );
+}
+
+Q3Block * Q3World::getBlockAtPos(glm::vec3 pos, bool ignoreAir)
+{
+    for (int x = 0; x < Q3_MAPSIZE; ++x) 
+    {
+        for (int y = 0; y < Q3_MAPSIZE; ++y) 
+        {
+            for (int z = 0; z < Q3_MAPSIZE; ++z) 
+            {
+                Q3Block *block = &(m_blocks[x][y][z]);
+
+                if (ignoreAir && block->type == Q3BlockType::Air) 
+                {
+                    continue;
+                }
+
+                glm::vec3 block_pos = block->position;
+                
+                // Simple AABB collision check.
+                bool point_is_inside_aabb = 
+                    pos.x > block_pos.x - 1.f && pos.x < block_pos.x + 1.f &&
+                    pos.y > block_pos.y - 1.f && pos.y < block_pos.y + 1.f &&
+                    pos.z > block_pos.z - 1.f && pos.z < block_pos.z + 1.f;
+
+                if (point_is_inside_aabb) 
+                {
+                    return &(m_blocks[x][y][z]);
+                }
+            }
         }
     }
-    else {
-        s_wasClicked = false;
-    }
+
+    /*
+    There is no block at the current position
+    or it is an air block and `ignoreAir` is set to true.
+    */
+    return nullptr;
 }
 
 void Q3WorldRenderer::render(Q3World * world, Q3Renderer *renderer)
 {
-    Q3Block *blocks = world->getBlocks();
-    for (int i = 0; i < Q3_MAPSIZE * Q3_MAPSIZE; ++i)
+    Q3World::Q3BlocksArray& blocks = world->getBlocks();
+
+    for (int x = 0; x < Q3_MAPSIZE; x++) 
     {
-        Q3Block *block = &(blocks[i]);
-        const glm::vec3& block_pos = block->position;
-        if(block->exists)
-            renderer->drawCube(block_pos.x, block_pos.y, block_pos.z, 1, 1, 1);
+        for (int y = 0; y < Q3_MAPSIZE; y++) 
+        {
+            for (int z = 0; z < Q3_MAPSIZE; z++) 
+            {
+                Q3Block *block = &(blocks[x][y][z]);
+                const glm::vec3& block_pos = block->position;
+
+                // We don't see air blocks therefore don't render them,
+                // (despite them still existing as block data)
+                if (block->type != Q3BlockType::Air) 
+                {
+                    renderer->drawCube(block_pos.x, block_pos.y, block_pos.z, 1, 1, 1);
+                }
+            }
+        }
     }
 }
