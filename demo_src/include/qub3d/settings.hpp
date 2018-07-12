839289a6f68ca529af00e7c3ec26f1653336081b
Vyom Fadia <vyom.fadia@outlook.com>
2018-07-12 20:11:54 +0100

    Add basic configuration loading.

--- demo_src/include/qub3d/settings.hpp
+++ demo_src/include/qub3d/settings.hpp
@@ -1,24 +1,45 @@
 #pragma once
 #include <string>
+#include <nlohmann/json.hpp>
 
 namespace settings
 {
 
-const int DEFAULTWINDOWWIDTH = 1280;
-const int DEFAULTWINDOWHEIGHT = 720;
+json *configurations
+{
+    using json = nlohmann::json;
 
-const std::string WINDOWTITLE = "Qub3d Demo";
-const std::string WINDOWTITLE_PAUSED = "Qub3d Demo - Paused";
+    std::ifstream file("assets/config.json"); 
+    std::string str((std::istreambuf_iterator<char>(file)),
+    std::istreambuf_iterator<char>());
+    configurations = json::parse(str);
+}
+
+int DEFAULTWINDOWWIDTH = configurations["display"]["width"];
+int DEFAULTWINDOWHEIGHT = config_file["display"]["height"];
+
+const std::string WINDOWTITLE = "Qub3d Demo"; // Title for SDL Window
+const std::string WINDOWTITLE_PAUSED = "Qub3d Demo - Paused"; // Title for Paused SDL Window
 
 struct GameConfig
 {
     int WindowWidth = DEFAULTWINDOWWIDTH;
     int WindowHeight = DEFAULTWINDOWHEIGHT;
 
-    static GameConfig *Get()
+    static GameConfig *Get() // Function for getting the Config data.
     {
-        static GameConfig c;
-        return &c;
+        static GameConfig config;
+        return &config;
+    }
+    
+    static GameConfig *AutoSet()
+    {
+        static GameConfig config;
+        
+        config->WindowWidth = configurations["display"]["width"];
+        config->WindowHeight = configurations["display"]["height"];
+
+        return &config
     }
 };
 
--- demo_src/src/main.cpp
+++ demo_src/src/main.cpp
@@ -1,11 +1,14 @@
 #include <qub3d/window.hpp>
+#include <qub3d/settings.hpp>
 
 int main(int argc, char **argv)
 {
-    int width = 640;
-    int height = 480;
+    // Get configurations and initialise variables
+    GameConfig *settings = CameConfig::Get();
+    
+    settings = CameConfig::AutoSet();
 
-    Window window("Qub3d", width, height);
+    Window window("Qub3d", settings->WindowWidth, settings->WindowHeight);
     window.pollEvents();
 
     while (window.isRunning())
