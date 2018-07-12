08b84f9e5321250c5eeb35856b09df18a48546af
Vyom Fadia <vyom.fadia@outlook.com>
2018-07-12 08:42:18 +0100

    Basic block data structuring

--- demo_src/include/qub3d/blocks.hpp
+++ demo_src/include/qub3d/blocks.hpp
@@ -1,9 +1,10 @@
-#pragma once
+#pragma once // Only be included once.
 
 #include <glm/glm.hpp>
+#include <string>
 
 // The type of Qube, the "property" of the block.
-namespace qubes
+namespace blocks
 {
     enum class QubeType
     {
@@ -12,5 +13,13 @@ namespace qubes
         Air, // Air, empty "blocks"
         Liquid, // Flowing stuff, probably unused for now.
         Collapsing // Collapsing blocks "fall" down, when blocks underneath it are removed
+    };
+
+    struct QubeData 
+    {
+        glm::vec3 position; // Position variable
+        std::string blockName; // name of the block
+        QubeType blockType = QubeType::Air; // Default block type is air
+        std::string textureName; // name of the texture file. and append "side", "top" & "bottom" in code
     };
 };
