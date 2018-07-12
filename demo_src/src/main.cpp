e8c46d05eccaac9e1672abda3831bb76eec8a553
Vyom Fadia <vyom.fadia@outlook.com>
2018-07-12 21:26:19 +0100

    beeperdeeper089 is very dumb, move things inside main.

--- demo_src/src/main.cpp
+++ demo_src/src/main.cpp
@@ -4,19 +4,19 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
 
-// Get configurations
-using json = nlohmann::json;
-std::ifstream file("assets/config.json"); 
-std::string str((std::istreambuf_iterator<char>(file)),
-std::istreambuf_iterator<char>());
-json config_file = json::parse(str);
-
-Settings::GameConfig *configurations = Settings::GameConfig::Get();
-configurations->WindowWidth = config_file["display"]["width"];
-configurations->WindowHeight = config_file["display"]["height"];
-
 int main(int argc, char **argv)
 {
+    // Get configurations
+    using json = nlohmann::json;
+    std::ifstream file("assets/config.json"); 
+    std::string str((std::istreambuf_iterator<char>(file)),
+    std::istreambuf_iterator<char>());
+    json config_file = json::parse(str);
+
+    Settings::GameConfig *configurations = Settings::GameConfig::Get();
+    configurations->WindowWidth = config_file["display"]["width"];
+    configurations->WindowHeight = config_file["display"]["height"];
+
 
     Window window("Qub3d", configurations->WindowWidth, configurations->WindowHeight);
     window.pollEvents();
